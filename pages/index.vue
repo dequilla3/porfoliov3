6e9aeac7a2709ec7c20be8f9da8963e695a5cad3
Kim Ariel Dequilla <deqkim4@gmail.com>
2023-10-17 21:42:08 +0800

    chore: added responsivenes

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,15 @@
       <!-- navigation -->
       <div class="absolute flex text-white">
         <!-- navbrand -->
-        <a href="/" class="py-10 px-20">
+        <a href="/" class="lg:py-10 lg:px-20">
           <div
-            class="brand shadow-lg shadow-pink-500/50 font-bold text-6xl px-4 pt-2 pb-5"
+            class="shadow-lg shadow-pink-500/50 font-bold lg:text-6xl text-4xl px-8 py-4"
           >
             KA.D
           </div>
         </a>
 
-        <ul class="flex font-mono py-16 pl-96">
+        <ul class="hidden lg:flex lg:float-right font-mono py-16">
           <li class="text-center text-2xl cursor-pointer mx-10 nav-item pb-1">
             home
           </li>
@@ -20,52 +20,46 @@
           <li class="text-center text-2xl cursor-pointer mx-10">projects</li>
         </ul>
       </div>
-      <!-- end navigation block -->
 
-      <!-- hero page title -->
-      <div class="absolute font-mono text-white top-64 left-40">
-        <label class="text-xl">Hi, my name is </label>
-        <label class="text-3xl">Kim Ariel N. Deqilla </label>
-        <br />
-        <!-- position -->
-        <div class="font-mono text-8xl pt-8 text-shadow">
-          Full Stack <br />
-          Web Developer
+      <div class="lg:h-screen bg-neutral-900">
+        <!-- hero page title -->
+        <div
+          class="pt-32 pl-5 lg:p-0 lg:absolute font-mono text-white lg:top-1/3 lg:left-40"
+        >
+          <label class="lg:text-xl text-sm">Hi, my name is </label>
+          <br />
+          <label class="lg:text-3xl text-xl">Kim Ariel N. Deqilla </label>
+          <br />
+          <!-- position -->
+          <div class="font-mono lg:text-8xl text-4xl lg:pt-8 text-shadow">
+            Full Stack <br />
+            Web Developer
+          </div>
         </div>
-      </div>
+        <div
+          class="absolute right-10 top-11 lg:w-64 lg:h-64 w-52 h-52 bg-pink-950 eclipse"
+        ></div>
 
-      <div class="absolute right-10 top-11 w-64 h-64 bg-pink-950 eclipse"></div>
+        <div
+          class="absolute right-60 top-60 lg:w-44 lg:h-44 w-36 h-36 bg-green-950 eclipse"
+        ></div>
 
-      <div
-        class="absolute right-60 top-60 w-44 h-44 bg-green-950 eclipse"
-      ></div>
-
-      <div
-        class="absolute right-96 top-96 w-28 h-28 bg-amber-200 eclipse"
-      ></div>
-
-      <div ref="hero" class="h-screen bg-neutral-900">
+        <div
+          class="absolute right-96 top-96 w-28 h-28 bg-amber-200 eclipse"
+        ></div>
         <img
-          class="absolute drop-shadow-xl right-24 bottom-0"
+          class="lg:absolute lg:min-h-screen drop-shadow-xl right-24 bottom-0"
           src="../assets/img/me2.png"
           alt=""
         />
       </div>
     </div>
-    <div ref="about" class="h-screen bg-neutral-600"></div>
   </div>
 </template>
 <script></script>
 <style scoped>
 .nav-item {
   border-bottom: 5px solid #74113a;
-}
-
-.brand {
-  border-top: 1px solid #006017;
-  border-right: 1px solid #006017;
-  border-bottom: 1px solid #006017;
-  border-left: 1px solid #006017;
 }
 
 .eclipse {
